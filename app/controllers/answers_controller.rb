1a2bc1bd80ef774ae1b77f92b25b8ac0c40378df
Leon Brimacombe <olibrimacombe@gmail.com>
2024-11-11 12:22:25 +0000

    Stupid coach rails MVC

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,16 @@
+class AnswersController < ApplicationController
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def coach_answer(question)
+    if question.downcase == "I am going to work"
+      return "Great!"
+    elsif question.end_with?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
